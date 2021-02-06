0b44c16bc6e086514dd4384124e56a8abe3f2518
MZandtheRaspberryPi <46413365+MZandtheRaspberryPi@users.noreply.github.com>
2021-02-05 21:18:40 -0800

    Update auto_watch.py

--- scripts/auto_watch.py
+++ scripts/auto_watch.py
@@ -50,7 +50,7 @@ def getkey():
     return key
 
 try:
-    font_path = "/home/mikey/Documents/Pi/nasalization-rg.ttf"
+    font_path = "~/auto_watch/nasalization-rg.ttf"
     text_size = 70
 
     text = PapirusTextPos(rotation=0)
