30eca753cee1c95861c178b4788e58ff88bbf66b
Michael Ziegltrum <ziegltrum@gmail.com>
2021-02-10 20:52:53 -0800

    updating display format for buses

--- scripts/auto_watch.py
+++ scripts/auto_watch.py
@@ -11,21 +11,18 @@ from check_weather import get_sf_weather
 from get_transit import get_northbound_arrivals, get_southbound_arrivals
 
 time.sleep(3)
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.INFO)
+ch = logging.StreamHandler()
+ch.setLevel(logging.DEBUG)
+formatter = logging.Formatter('%(asctime)s - %(levelname)s - %(message)s')
+ch.setFormatter(formatter)
+logger.addHandler(ch)
 if os.path.exists(r"/home/mikey/logs/watch.log"):
-    ch = logging.StreamHandler()
-    ch.setLevel(logging.DEBUG)
-    formatter = logging.Formatter('%(asctime)s - %(levelname)s - %(message)s')
-    ch.setFormatter(formatter)
-    logger.addHandler(ch)
-    logging.basicConfig(filename=r'/home/mikey/logs/watch.log',
-                        level=logging.INFO,
-                        format=' %(asctime)s - %(levelname)s - %(message)s',
-                        handlers=[ch])
-
-else:
-    print("not file watcher")
-    logging.basicConfig(level=logging.INFO,
-                        format=' %(asctime)s - %(levelname)s - %(message)s')
+    fh = logging.FileHandler(filename=r"/home/mikey/logs/watch.log")
+    fh.setLevel(logging.DEBUG)
+    fh.setFormatter(formatter)
+    logger.addHandler(fh)
 
 my_511_token = sys.argv[1]
 
@@ -52,20 +49,35 @@ weather_button = Button(SW4, pull_up=False)
 button5 = Button(SW5, pull_up=False)
 
 def display_arivals(text, arrivals):
-    logging.info("arivals {}".format(arrivals))
+    logger.info("arivals {}".format(arrivals))
     text.Clear()
-    for stop in sorted(arrivals.keys(), reverse=True):
-        text1 = stop
-        for route in arrivals[stop].keys():
-            # truncating routes w/ long names, for bart with long ones
-            text2 = (route[:5] + '.:') if len(route) > 5 else route + ":"
-            # appending arrival times for each route
-            for arrival in arrivals[stop][route]:
-                text2 += " "
-                text2 += str(arrival)
-            text.AddText("{}\n{}".format(text1, text2), size=15, fontPath=font_path)
-            time.sleep(5)
-            text.Clear()
+    # example data
+    # {'CivCtr': {'YL-N': [20, 50], 'RD-N': [12, 42], 'GN-N': [22, 37], 'BL-N': [2, 32]}, 'Missn&11': {'14R': [7, 13, 21], '14': [5, 7, 30]}}
+    arrival_str_miss = "Miss&11" + "\n"
+    arrival_str_civ = 'CivCtr' + "\n"
+    civ_ctr_arrivals = sorted(arrivals["CivCtr"].keys(), reverse=True)
+    miss_arrivals = sorted(arrivals["Missn&11"].keys(), reverse=True)
+
+    for civ_arrival in civ_ctr_arrivals:
+        arrival_times = [str(arriv) for arriv in arrivals["CivCtr"][civ_arrival]]
+        arrival_times = ", ".join(arrival_times)
+        str_to_add = civ_arrival + ": " +  arrival_times
+        arrival_str_civ += str_to_add + "\n"
+
+    for miss_arrival in miss_arrivals:
+        arrival_times = [str(arriv) for arriv in arrivals["Missn&11"][miss_arrival]]
+        arrival_times = ", ".join(arrival_times)
+        str_to_add = miss_arrival + ": " + arrival_times
+        arrival_str_miss += str_to_add + "\n"
+    logger.info(arrival_str_miss)
+    logger.info(arrival_str_civ)
+    text.AddText(arrival_str_miss, size=15, fontPath=font_path, Id="Miss")
+    time.sleep(3)
+    text.RemoveText("Miss")
+    text.AddText(arrival_str_civ, size=15, fontPath=font_path, Id="Civ")
+    time.sleep(3)
+    text.RemoveText("Civ")
+
 
 def setkey(device):
     global key
@@ -93,7 +105,7 @@ try:
     old_time = datetime.datetime.now().strftime("%H:%M")
     text.AddText(old_time, size=text_size, fontPath=font_path)
     time.sleep(10)
-    logging.info("Entering watch loop")
+    logger.info("Entering watch loop")
     while True:
         # Define button press action (Note: inverted logic w.r.t. gpiozero)
         shutdown_button.when_released = setkey
@@ -114,7 +126,7 @@ try:
             old_time = new_time
             time.sleep(.3)
         elif key == "shutdown":
-            logging.info("shutting down")
+            logger.info("shutting down")
             text.Clear()
             text.AddText("Bye :)", size=text_size, fontPath=font_path)
             time.sleep(2)
@@ -126,19 +138,19 @@ try:
             subprocess.Popen(['sudo', '{}/shutdown.sh'.format(shutdown_path)])
             sys.exit()
         elif key == "n_transit":
-            logging.info("getting n_transit")
+            logger.info("getting n_transit")
             n_transit = get_northbound_arrivals(my_511_token=my_511_token)
             display_arivals(text, n_transit)
             key = 'none'
             old_time = "fake time"
         elif key == "s_transit":
-            logging.info("getting s_transit")
+            logger.info("getting s_transit")
             s_transit = get_southbound_arrivals(my_511_token)
             display_arivals(text, s_transit)
             key = 'none'
             old_time = "fake time"
         elif key == "weather":
-            logging.info("getting weather")
+            logger.info("getting weather")
             weather = get_sf_weather()
             text.Clear()
             text.AddText(weather, size=15, fontPath=font_path)
@@ -146,7 +158,7 @@ try:
             key = 'none'
             old_time = "fake time"
         elif key == "button5":
-            logging.info("button 5 pressed")
+            logger.info("button 5 pressed")
             key = 'none'
 
 except Exception as e:
